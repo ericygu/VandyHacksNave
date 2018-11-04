0503a315ec274b736f2017bfcb72c0cbef579f2e
Sriharsha Singam <harshasingam3@gmail.com>
2018-11-03 20:49:16 -0400

    new-website-using-bootstrap

---
+++ public/index.js
@@ -0,0 +1,23 @@
+
+var currentStateVal = 0;
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.calendar').datepicker();
+    $('.dropdown-menu a').on('click', function(){  
+        console.log($(this).data("value"));
+        currentStateVal = $(this).data("value");
+        $('.dropdown-toggle').html($(this).html());    
+    })
+    $.getJSON("https://vandyhacks-1541273118069.firebaseapp.com/StoreLocator.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
+//    console.log(csvJSON());
+
+});
+var map;
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+}
